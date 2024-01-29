735f53fc43cd1eaf35bb9a0bece590b27fb522c0
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2024-01-28 23:07:33 -0500

    Now we only show recipes and shoppingList on selecting either of them from the menu bar

--- recipie-book-project/src/app/app.component.ts
+++ recipie-book-project/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public feature: string;
 
+  recipesClicked(feature: string): void {
+    this.feature = feature;
+  }
 
 }
--- recipie-book-project/src/app/header/header.component.ts
+++ recipie-book-project/src/app/header/header.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+  @Output() isFeatureClicked: EventEmitter<string> = new EventEmitter<string>();
+
+  public featureClicked(feature: string): void {
+    this.isFeatureClicked.emit(feature);
+  }
 
 }
