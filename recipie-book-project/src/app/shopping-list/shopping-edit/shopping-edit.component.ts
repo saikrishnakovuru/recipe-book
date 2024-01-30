a5a0e5cfc9a86d5262516ff29b5ce83e130805cb
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2024-01-29 22:10:01 -0500

    Passed data from ShoppingEditComponent to ShoppingListComponent

--- recipie-book-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipie-book-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  public ingredientName: string;
+  public ingredientQuantity: string;
+  @Output() passIngredients: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public onAddButtonClick(ingredientName: HTMLInputElement, ingredientQuantity: HTMLInputElement) {
+    this.ingredientName = ingredientName.value;
+    this.ingredientQuantity = ingredientQuantity.value;
+    if (this.ingredientName !== '' && this.ingredientQuantity !== '') {
+      this.passIngredients.emit(new Ingredient(this.ingredientName, +this.ingredientQuantity));
+    }
   }
 
 }
--- recipie-book-project/src/app/shopping-list/shopping-list.component.ts
+++ recipie-book-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { Ingredient } from '../shared/ingredient.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -17,5 +17,10 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
 
 }
