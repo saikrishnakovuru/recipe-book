17beb26535e383deeac2382930b8ba611ab0de9c
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2024-01-30 23:31:08 -0500

    Implemented a custom directive to expand the dropdowns in headerComponent and recipeDetailComponent

--- recipie-book-project/src/app/app.module.ts
+++ recipie-book-project/src/app/app.module.ts
@@ -11,6 +11,7 @@ import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.compo
 import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
+import {DropdownDirective} from "./shared/dropdown.directive";
 
 @NgModule({
   declarations: [
@@ -21,11 +22,12 @@ import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
